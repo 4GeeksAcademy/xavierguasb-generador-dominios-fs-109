dcbcfee95c0d3c2c8d8ce6739dcbcddb5fda6e53
Xavier Guas <xavier.guas@gmail.com>
2025-05-16 18:37:30 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -5,28 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
 
-let pronoun = ['el', 'la', 'lo'];
-let adj = ['mejor', 'gran'];
-let noun = ['sitio', 'lugar', 'ciudad'];
-let dot = ['.es', '.com', '.org'];
-  
+  let pronoun = ['el', 'la'];
+  let adjetive = ['mejor', 'gran'];
+  let noun = ['sitio', 'lugar', 'ciudad'];
+  let extension = ['.es', '.com', '.org'];
 
   for (let a = 0; a < pronoun.length; a++) {
-    for (let b = 0; b < adj.length; b++){
-      for (let c =0; c < noun.length; c++){
-        for (let d =0; d < dot.length; d++){
+    for (let b = 0; b < adjetive.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < extension.length; d++) {
 
-console.log(pronoun[a] + adj[b] + noun[c] + dot[d])
+          console.log(pronoun[a] + adjetive[b] + noun[c] + extension[d])
 
+          // let domain = pronoun[a] + adjetive[b] + noun[c] + extension[d];
+
+          // let finalDomains = document.getElementById("domains");
+          //     finalDomains.innerHTML = domain;
 
         }
       }
     }
   }
-
 };
 
